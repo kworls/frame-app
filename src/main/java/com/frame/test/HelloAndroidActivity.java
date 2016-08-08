12b80b8f52519d98a0f963811a5759a0a9949463
Frame <zfeng1648@hotmail.com>
2016-08-09 00:06:05 +0800

    2

--- src/main/java/com/frame/test/HelloAndroidActivity.java
+++ src/main/java/com/frame/test/HelloAndroidActivity.java
@@ -13,6 +13,7 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static com.frame.utils.getCpuInfo.getProcessCpuRate;
 import static java.lang.Runtime.getRuntime;
 
 public class HelloAndroidActivity extends Activity {
@@ -29,41 +30,42 @@ public class HelloAndroidActivity extends Activity {
                 TextView textv =(TextView)findViewById(R.id.textView1);
                 textv.setMovementMethod(new ScrollingMovementMethod());
                 Toast.makeText(HelloAndroidActivity.this,"Clicked",Toast.LENGTH_SHORT).show();
-//                textv.setText("asdasdasd");
+                textv.setText("asdasdasd");
 
-//                String Result;
-//                Process p= null;
-//                try {
+                String Result;
+                Process p= null;
+                try {
 //                    p = getRuntime().exec("top -d 1 -n 1");
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//
-//                BufferedReader br=new BufferedReader(new InputStreamReader
-//                        (p.getInputStream ()));
-//                try {
-//                    while((Result=br.readLine())!=null)
-//                    {
-//
-//                        if(Result.trim().length()<1){
-//                            continue;
-//                        }else{
-//                            String sa=Result.toString();
-//                            textv.append(sa+"asd");
-//
-//                            //                            String[] CPUusr = Result.split("%");
-////                            textv.append("USER:"+CPUusr[0]+"\n");
-////                            String[] CPUusage = CPUusr[0].split("User");
-////                            String[] SYSusage = CPUusr[1].split("System");
-////                            textv.append("CPU:"+CPUusage[1].trim()+" length:"+CPUusage[1].trim().length()+"\n");
-////                            textv.append("SYS:"+SYSusage[1].trim()+" length:"+SYSusage[1].trim().length()+"\n");
-////                            textv.append(Result+"\n");
-//                            break;
-//                        }
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
+                    p = getRuntime().exec("ps");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                BufferedReader br=new BufferedReader(new InputStreamReader
+                        (p.getInputStream ()));
+                try {
+                    while((Result=br.readLine())!=null)
+                    {
+
+                        if(Result.trim().length()<1){
+                            continue;
+                        }else{
+                            String sa=Result.toString();
+                            textv.append(sa+"asd");
+
+                            //                            String[] CPUusr = Result.split("%");
+//                            textv.append("USER:"+CPUusr[0]+"\n");
+//                            String[] CPUusage = CPUusr[0].split("User");
+//                            String[] SYSusage = CPUusr[1].split("System");
+//                            textv.append("CPU:"+CPUusage[1].trim()+" length:"+CPUusage[1].trim().length()+"\n");
+//                            textv.append("SYS:"+SYSusage[1].trim()+" length:"+SYSusage[1].trim().length()+"\n");
+//                            textv.append(Result+"\n");
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
 
                 float f=getProcessCpuRate();
@@ -82,70 +84,6 @@ public class HelloAndroidActivity extends Activity {
 
 
 
-    public static float getProcessCpuRate()
-    {
 
-        float totalCpuTime1 = getTotalCpuTime();
-        float processCpuTime1 = getAppCpuTime();
-        try
-        {
-            Thread.sleep(50);
-
-        }
-        catch (Exception e)
-        {
-        }
-
-        float totalCpuTime2 = getTotalCpuTime();
-        float processCpuTime2 = getAppCpuTime();
-
-        float cpuRate = 100 * (processCpuTime2 - processCpuTime1)
-                / (totalCpuTime2 - totalCpuTime1);
-
-        return cpuRate;
-    }
-    public static long getTotalCpuTime()
-    {   // 获取系统总CPU使用时间
-        String[] cpuInfos = null;
-        try
-        {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    new FileInputStream("/proc/stat")), 1000);
-            String load = reader.readLine();
-            reader.close();
-            cpuInfos = load.split(" ");
-        }
-        catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-        long totalCpu = Long.parseLong(cpuInfos[2])
-                + Long.parseLong(cpuInfos[3]) + Long.parseLong(cpuInfos[4])
-                + Long.parseLong(cpuInfos[6]) + Long.parseLong(cpuInfos[5])
-                + Long.parseLong(cpuInfos[7]) + Long.parseLong(cpuInfos[8]);
-        return totalCpu;
-    }
-
-    public static long getAppCpuTime()
-    {   // 获取应用占用的CPU时间
-        String[] cpuInfos = null;
-        try
-        {
-            int pid = android.os.Process.myPid();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    new FileInputStream("/proc/" + pid + "/stat")), 1000);
-            String load = reader.readLine();
-            reader.close();
-            cpuInfos = load.split(" ");
-        }
-        catch (IOException ex)
-        {
-            ex.printStackTrace();
-        }
-        long appCpuTime = Long.parseLong(cpuInfos[13])
-                + Long.parseLong(cpuInfos[14]) + Long.parseLong(cpuInfos[15])
-                + Long.parseLong(cpuInfos[16]);
-        return appCpuTime;
-    }
 }
 
